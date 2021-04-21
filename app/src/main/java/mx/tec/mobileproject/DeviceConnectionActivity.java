327a2ffc0ae210ed6b9e5f5c1776521de710e4b8
Antonio Perez <00antonio.perez00@gmail.com>
2021-04-21 18:23:07 -0500

    feat: intent to display devices

--- app/src/main/java/mx/tec/mobileproject/DeviceConnectionActivity.java
+++ app/src/main/java/mx/tec/mobileproject/DeviceConnectionActivity.java
@@ -2,13 +2,84 @@ package mx.tec.mobileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothHeadset;
+import android.bluetooth.BluetoothProfile;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
+import java.util.Set;
 
 public class DeviceConnectionActivity extends AppCompatActivity {
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String logger = "connection.activity";
         setContentView(R.layout.activity_device_connection);
+        int REQUEST_ENABLE_BT = 1;
+
+        ListView pairedList =  findViewById(R.id.pairedDevicesList);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, R.layout.bluetooth_item);
+        pairedList.setAdapter(adapter);
+
+
+        ((Button) findViewById(R.id.searchButton)).setOnClickListener(v -> {
+            if (bluetoothAdapter == null) {
+                String error = "The device do not support bluetooth";
+                Log.d(logger, error);
+                return;
+            }
+
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+
+
+            Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+            if (pairedDevices.size() > 0) {
+                // There are paired devices. Get the name and address of each paired device.
+                for (BluetoothDevice device : pairedDevices) {
+                    String deviceName = device.getName();
+                    String deviceHardwareAddress = device.getAddress(); // MAC address
+                    Log.d(logger, deviceName);
+                    Log.d(logger, deviceHardwareAddress);
+
+
+                    adapter.add(deviceName + " " + deviceHardwareAddress);
+                }
+            }
+
+        });
+
+
     }
+
 }
