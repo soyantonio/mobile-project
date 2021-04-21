468774b46bfc49d7ce8e8ec1939563f1ac77426e
Antonio Perez <00antonio.perez00@gmail.com>
2021-04-21 17:17:06 -0500

    feat: add blank connection to bluetooth

---
+++ app/src/main/java/mx/tec/mobileproject/DeviceConnectionActivity.java
@@ -0,0 +1,14 @@
+package mx.tec.mobileproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeviceConnectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_connection);
+    }
+}
--- app/src/main/java/mx/tec/mobileproject/MainActivity.java
+++ app/src/main/java/mx/tec/mobileproject/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.statusTextView)).setText("Estado: Carrito Desconectado :(");
         String logger = "main.activity";
 
         ((Button) findViewById(R.id.backButton)).setOnClickListener(v -> {
