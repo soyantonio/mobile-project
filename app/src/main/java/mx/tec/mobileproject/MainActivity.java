591beac39984bda1189ec619045bf41362494ba5
Antonio Perez <00antonio.perez00@gmail.com>
2021-04-21 17:07:51 -0500

    chore: add button listeners

--- app/src/main/java/mx/tec/mobileproject/MainActivity.java
+++ app/src/main/java/mx/tec/mobileproject/MainActivity.java
@@ -3,6 +3,8 @@ package mx.tec.mobileproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String logger = "main.activity";
+
+        ((Button) findViewById(R.id.backButton)).setOnClickListener(v -> {
+            Log.d(logger, "Back");
+        });
+
+        ((Button) findViewById(R.id.frontButton)).setOnClickListener(v -> {
+            Log.d(logger, "Front");
+        });
+
+        ((Button) findViewById(R.id.leftButton)).setOnClickListener(v -> {
+            Log.d(logger, "Left");
+        });
+
+
+        ((Button) findViewById(R.id.rightButton)).setOnClickListener(v -> {
+            Log.d(logger, "Right");
+        });
+
+
+
+        // f, b, l, r <- Bluetooth Commands
+
     }
 }
